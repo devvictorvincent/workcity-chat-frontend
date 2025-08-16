8ea14026b4c17540bedb785e6132998477b5452c
Victor Vincent <dev.victorvincent@gmail.com>
2025-08-16 01:36:28 +0100

    Setting up project Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { io } from "socket.io-client";  
+import AdminDashboard from "./pages/admin/index.jsx";
+import Login from "./pages/auth/login.jsx";
+import Signup from "./pages/auth/signup.jsx";
+
+// Change this to your backend URL
+const socket = io("http://localhost:5000");
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} /> 
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/admin" element={<AdminDashboard />} /> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
